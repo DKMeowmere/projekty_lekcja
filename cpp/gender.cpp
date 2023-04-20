03e2f782e215cde816063e13d864592e8508063f
Dawid Wiselka <dawo4444dawo@gmail.com>
2023-04-20 13:50:40 +0200

    add gender file

---
+++ cpp/gender.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  char gender;
+   cout << "Wpisz litere m jesli jeśteś mezczyzna, k jesli jestes kobieta lub inna jesli jestes no gender" << endl;
+  cin >> gender; 
+
+  if(gender == 'm') cout << "jestes mezczyzna" << endl;
+  else if(gender == 'k') cout << "jestes kobieta" << endl;
+  else cout << "no gender" << endl;
+
+  return 0;
+}
